ba57ec5fa23738b332408b9f60455bfbd481be7c
AmitSheth9 <amit.sheth9@gmail.com>
2021-08-23 15:45:43 -0700

    products created as object iterals added to array

---
+++ products/teas.js
@@ -0,0 +1,67 @@
+
+
+const greenTea = {
+    id: 'green',
+    name: 'Green Tea',
+    image: greentea.jpeg,
+    description: 'High in antioxidants, improves brain function ',
+    category: 'healthy',
+    price: 5
+
+};
+
+const earlGreyTea = {
+    id: 'earlgrey',
+    name: 'Earl Grey Tea',
+    image: earlgrey.jpeg,
+    description: 'Black tea combined with bergamot citrus extract',
+    category: 'black',
+    price: 3
+};
+const chaiTea = {
+    id: 'chai',
+    name: 'Chai Tea',
+    image: chai.png,
+    description: 'Black tea with variety of spices and herbs mixed with milk and sugar',
+    category: 'black',
+    price: 4
+};
+
+const chamomileTea = {
+    id: 'chamomile',
+    name: 'Chamomile Tea',
+    image: chamomile.png,
+    description: 'Herbal tea made from Chamomile flowers used for reducing anxiety and helping sleep.',
+    category: 'herbal',
+    price: 3.50
+};
+
+const oolongTea = {
+    id: 'oolong',
+    name: 'Oolong Tea',
+    image: oolong.png,
+    description:'Traditional Chinese tea that protect against Diabetes',
+    category: 'Chinese',
+    price: 3
+};
+
+const englishBreakfastTea = {
+    id: 'breakfast',
+    name: 'English Breakfast Tea',
+    image: english.png,
+    description: 'Rich Assam black tea enjoyed with milk.',
+    category: 'black',
+    price: 4
+
+};
+
+const herbalTea = {
+    id: 'herbal',
+    name: 'Herbal Tea',
+    image: herbal.png,
+    description: 'A mix of different herbs that fights cold, boosts immune system, and improves digestion',
+    category: 'herbal',
+    price: 2.50
+};
+
+export let teaArray = [greenTea, earlGreyTea, chaiTea, chamomileTea, oolongTea, englishBreakfastTea, herbalTea];
