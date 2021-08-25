ffa29bedb2a9fa8d175534cbc6d6ae0addffb091
AmitSheth9 <amit.sheth9@gmail.com>
2021-08-25 10:55:12 -0700

    added more working tests

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,32 @@
 import { renderCart } from '../cart/render-line-items.js';
 import { cartArray } from '../cart/cart.js';
+import { calcOrderTotal, findById } from '../cart/utils.js';
+import { teaArray } from '../products/teas.js';
 
 const test = QUnit.test;
+
+test('test findById to see if id102 returns correct product', (expect) => {
+    const expected = {
+        id: 102,
+        name: 'Earl Grey Tea',
+        image: '../assets/earlgrey2.jpeg',
+        description: 'Black tea combined with bergamot citrus extract',
+        category: 'black',
+        price: 3.99
+    };
+
+    const actual = findById(teaArray, 102);
+
+    expect.deepEqual(actual, expected);
+});
+
+test('calcOrderTotal to see if order total is returneed', (expect) => {
+    const expected = "$44.41";
+
+    const actual = calcOrderTotal(cartArray, teaArray);
+
+    expect.equal(actual, expected);
+});
 
 test('test if renderTea outputs the same format as static HTML', (expect) => {
 
